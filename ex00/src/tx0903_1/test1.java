c77d4d86814fb6be9360bd96207e52da154ecffe
star <alskfl7777@naver.com>
2026-09-03 18:15:55 +0900

    version 0.02
    test update (retry)

---
+++ ex00/src/tx0903_1/test1.java
@@ -0,0 +1,15 @@
+package tx0903_1;
+
+public class test1 {
+	public static void main(String[] args) {
+		int[] ito2 = new int[3];
+		int count =0;
+		
+		for(int i : ito2) {
+			ito2[count] = (count + 1) *10;
+			int temp = ito2[count];
+			System.out.println(temp);
+			count ++;
+		}
+	}
+}
